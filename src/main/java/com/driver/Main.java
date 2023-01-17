fec0b6fed55db5c927a9b09aa50ef142d4af11f2
Abhishek Kumar <ak207326@gmail.com>
2023-01-17 14:18:23 +0530

    Solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+//        obj.name() = kunal; wont work as private
+        obj.setName("Kunal");
+        obj.getName();
+    }
   
 }
